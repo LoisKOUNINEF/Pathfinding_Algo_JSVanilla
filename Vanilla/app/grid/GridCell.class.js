94fa2c20e1eec0c84971a1595ba2cf9a997e7345
LoisKOUNINEF <lkouninef@gmail.com>
2022-05-02 16:52:15 +0200

    clean gridcell from events

--- Vanilla/app/grid/GridCell.class.js
+++ Vanilla/app/grid/GridCell.class.js
@@ -1,4 +1,5 @@
 import { generateQueryConstructor } from '../utils/object.utils.js'
+import renderEvents from './gridcell/gridcell-events.method.js'
 
 class GridCell {
 
@@ -10,19 +11,15 @@ class GridCell {
     return `${this.row}|${this.col}`
   }
 
-  get gridCellElement() {
-    return document.querySelector(`.gridcell[position="${ this.position }"]`)
-  }
-
   render() {
-    this.#renderElement()
-    this.#renderGridCell()
-    this.#renderHtml()
-    this.renderOutInCells()
-    this.#renderEvents()
+    this.#renderHtmlElement()
+    this.#renderHtmlStyling()
+    this.#renderAttribute()
+    this.renderGridCellDynamics()
+    renderEvents.call(this)
   }
 
-  #renderElement() {
+  #renderHtmlElement() {
     const {grid: { gridElement } } = this
     const gridCellElement = document.createElement('div')
 
@@ -30,17 +27,10 @@ class GridCell {
     gridCellElement.setAttribute('position', this.position)
 
     gridElement.append(gridCellElement)
+    this.gridCellElement = gridCellElement
   }
 
-  #renderGridCell() {
-    const { grid: { numberColumns, numberRows } } = this
-
-    this.isBlocked = false
-    this.isOutCell = this.position === '0|0'
-    this.isInCell = this.position === `${ numberRows - 1}|${ numberColumns - 1 }`
-  }
-
-  #renderHtml() {
+  #renderHtmlStyling() {
     const { gridCellElement, grid: { settings: { cellSize, borderSize, borderColor } } } = this
 
     Object.assign( gridCellElement.style, {
@@ -52,89 +42,18 @@ class GridCell {
     gridCellElement.setAttribute('draggable', true)
   }
 
-  renderOutInCells() {
+  #renderAttribute() {
+    const { grid: { numberColumns, numberRows } } = this
+
+    this.isBlocked = false
+    this.isOutCell = this.position === '0|0'
+    this.isInCell = this.position === `${ numberRows - 1}|${ numberColumns - 1 }`
+  }
+
+  renderGridCellDynamics() {
     this.gridCellElement.classList[ this.isOutCell ? 'add' : 'remove' ]( 'out-cell' )
     this.gridCellElement.classList[ this.isInCell ? 'add' : 'remove' ]( 'in-cell' )
-  }
-
-  renderBlockedCells() {
     this.gridCellElement.classList[ this.isBlocked ? 'add' : 'remove' ]( 'blocked' )
-  }
-
-  #renderEvents() {
-    this.#renderClickEvent()
-    this.#renderHoverEvent()
-    this.#renderDragDropEvent()
-  }
-
-  #renderClickEvent() {
-    const { gridCellElement } = this
-
-    gridCellElement.addEventListener('click', _ => {
-      if (this.isOutCell || this.isInCell ) return
-        this.isBlocked = !this.isBlocked
-      this.renderBlockedCells()
-    })
-  }
-
-  #renderHoverEvent() {
-    const { gridCellElement } = this
-
-    gridCellElement.addEventListener( 'mouseover', _ => {
-      if (this.isOutCell || this.isInCell ) {
-        gridCellElement.style.cursor = 'grab'
-      }
-      else if ( !this.isBlocked ) {
-        gridCellElement.style.cursor = 'pointer'
-      }
-      else {
-        gridCellElement.style.cursor = 'crosshair'
-      }
-    })
-  }
-
-  #renderDragDropEvent() {
-    const { gridCellElement, grid } = this
-
-    gridCellElement.addEventListener('dragover', event => {
-      if ( dontAllowDrop.call( this ) ) return
-        event.preventDefault()
-    })
-
-    gridCellElement.addEventListener('dragstart', event => {
-      if ( dontAllowDrag.call( this ) ) {
-        event.preventDefault()
-        return
-      }
-      grid.draggedGridCell = this
-    })
-
-    gridCellElement.addEventListener('drop', _ => {
-      this.resetCell()
-
-      this.isOutCell = grid.draggedGridCell.isOutCell
-      this.isInCell = grid.draggedGridCell.isInCell
-
-      this.renderOutInCells()
-
-      grid.draggedGridCell.resetCell()
-      // grid.draw()
-    })
-
-    function dontAllowDrop() {
-      const { gridCellElement, grid } = this
-
-      if ( grid.draggedGridCell.gridCellElement === gridCellElement ) return true
-      if ( grid.draggedGridCell.isOutCell && this.isInCell ) return true
-      if ( grid.draggedGridCell.isInCell && this.isOutCell ) return true
-
-      return false
-    }
-
-    function dontAllowDrag() {
-      return ( !this.isOutCell && !this.isInCell )
-    }
-
   }
 
   resetCell() {
@@ -142,8 +61,7 @@ class GridCell {
     this.isOutCell = false
     this.isBlocked = false
 
-    this.renderOutInCells()
-    this.renderBlockedCells()
+    this.renderGridCellDynamics()
   }
 
 }
---
+++ Vanilla/app/grid/gridcell/gridcell-events.method.js
@@ -0,0 +1,77 @@
+export default function () {
+
+  renderClickEvent.call(this)
+  renderHoverEvent.call(this)
+  renderDragDropEvent.call(this)
+
+}
+
+function renderClickEvent() {
+  const { gridCellElement } = this
+
+  gridCellElement.addEventListener('click', _ => {
+    if ( this.isOutCell || this.isInCell ) return
+      this.isBlocked = !this.isBlocked
+    this.renderGridCellDynamics()
+  })
+}
+
+function renderHoverEvent() {
+  const { gridCellElement } = this
+
+  gridCellElement.addEventListener( 'mouseover', _ => {
+    if (this.isOutCell || this.isInCell ) {
+      gridCellElement.style.cursor = 'grab'
+    }
+    else if ( !this.isBlocked ) {
+      gridCellElement.style.cursor = 'pointer'
+    }
+    else {
+      gridCellElement.style.cursor = 'crosshair'
+    }
+  })
+}
+
+function renderDragDropEvent() {
+  const { gridCellElement, grid } = this
+
+  gridCellElement.addEventListener('dragover', event => {
+    if ( dontAllowDrop.call( this ) ) return
+      event.preventDefault()
+  })
+
+  gridCellElement.addEventListener('dragstart', event => {
+    if ( dontAllowDrag.call( this ) ) {
+      event.preventDefault()
+      return
+    }
+    grid.draggedGridCell = this
+  })
+
+  gridCellElement.addEventListener('drop', _ => {
+    this.resetCell()
+
+    this.isOutCell = grid.draggedGridCell.isOutCell
+    this.isInCell = grid.draggedGridCell.isInCell
+
+    this.renderGridCellDynamics()
+
+    grid.draggedGridCell.resetCell()
+      // grid.draw()
+    })
+
+}
+
+function dontAllowDrop() {
+  const { gridCellElement, grid } = this
+
+  if ( grid.draggedGridCell.gridCellElement === gridCellElement ) return true;
+  if ( grid.draggedGridCell.isOutCell && this.isInCell ) return true;
+  if ( grid.draggedGridCell.isInCell && this.isOutCell ) return true;
+
+  return false
+}
+
+function dontAllowDrag() {
+  return ( !this.isOutCell && !this.isInCell )
+}
