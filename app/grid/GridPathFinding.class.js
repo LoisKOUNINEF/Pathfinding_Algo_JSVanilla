6ae9df60af6c13169edcd15b78390338ce9befc2
LoisKOUNINEF <lkouninef@gmail.com>
2022-05-10 19:23:06 +0200

    import algorithm and grid

--- app/grid/GridPathFinding.class.js
+++ app/grid/GridPathFinding.class.js
@@ -1,4 +1,9 @@
 import { generateQueryConstructor } from '../utils/object.utils.js'
+import AStarFinder from '../lib/pathfinding/AStarFinder.js'
+import PathFindingGrid from '../lib/pathfinding/Grid.js'
+
+window.AStarFinder = AStarFinder
+window.PathFindingGrid = PathFindingGrid
 
 class GridPathFinding {
 
